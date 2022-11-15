c9a885b9a7b8dddf2e185492838f6d7bbd4cf520
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-11-14 21:15:51 -0300

    Ok

--- ExercicioApi/Program.cs
+++ ExercicioApi/Program.cs
@@ -1,14 +1,25 @@
 using ExercicioApi.Contracts.v1;
+using ExercicioApi.Models.v1;
 using ExercicioApi.Repositories.v1;
 using ExercicioApi.Utils.v1;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
-//builder.Services.AddScoped<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
-
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
+builder.Services.AddScoped<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 builder.Services.AddScoped<IContractRepository, ContractRepository>();
+
+
+//COMO FARIA NORMALMENTE***********************************
+//DatabaseSettings _settings = new();
+//var contracts = new MongoClient(_settings.ConnectionString);
+//var database = contracts.GetDatabase(_settings.DatabaseName);
+//IMongoCollection<Contract> collection = database.GetCollection<Contract>(_settings.ContractCollectionName);
+//COMO FARIA NORMALMENTE***********************************
+
+
 
 builder.Services.AddControllers();
 
