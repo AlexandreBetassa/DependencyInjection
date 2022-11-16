91ea464c212d31d93aa580dd43f329a018e3c4f4
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-11-16 08:54:00 -0300

    Refresh

--- ExercicioApi/Program.cs
+++ ExercicioApi/Program.cs
@@ -3,15 +3,29 @@ using ExercicioApi.Repositories.v1;
 using ExercicioApi.Serivce.v11;
 using ExercicioApi.Utils.v1;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 builder.Services.AddScoped<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+builder.Services.AddMongoDb();
 
 builder.Services.AddScoped<IContractService, ContractService>();
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped(typeof(IMongoClient), typeof(MongoClient));
 
+//=====================
+
+//builder.Services.AddSingleton<IMongoClient>(c =>
+//{
+//    return new MongoClient(
+//        string.Format("mongodb://localhost:27017"));
+//});
+
+//builder.Services.AddScoped(c => c.GetService<IMongoClient>().StartSession());
+
+//==============
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ExercicioApi/Utils/v1/Service.cs
+++ ExercicioApi/Utils/v1/Service.cs
@@ -1,6 +1,22 @@
-﻿namespace ExercicioApi.Utils.v1
+﻿using ExercicioApi.Contracts.v1;
+using ExercicioApi.Models.v1;
+using ExercicioApi.Repositories.v1;
+using MongoDB.Driver;
+
+namespace ExercicioApi.Utils.v1
 {
-    public class Service
+    public static class Service
     {
+        public static IServiceCollection AddMongoDb(this IServiceCollection services)
+        {
+            var connectionString = MongoClientSettings.FromConnectionString("mongodb://localhost:27017");
+            var mongoClient = new MongoClient(connectionString);
+            var database = mongoClient.GetDatabase("ContractsDb");
+            var collection = database.GetCollection<Contract>(nameof(Contract));
+
+            services.AddSingleton<IMongoClient>(mongoClient); //uma unica instancia enquanto a aplicacao esta rodando
+            services.AddScoped<IBaseRepository<Contract>, BaseRepository<Contract>>(); //cada request que for efetuada ele cria uma nova conexao
+            return services;
+        }
     }
 }
